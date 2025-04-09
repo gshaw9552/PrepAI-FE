846a0418842a0e2d6877e668c6f8be0ce2631e64
gshaw9552 <gshaw9552@gmail.com>
2025-04-09 11:55:05 +0530

    fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App: React.FC = () => {
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            {/* <Route path="/register" element={<Register />} /> */}
             <Route path="/dashboard" element={<Dashboard />} />
             <Route 
               path="/analytics" 
